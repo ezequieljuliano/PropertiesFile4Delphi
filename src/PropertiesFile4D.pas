11cf1f0c383b350c5bb9351cbf06ed80ab41c671
Ezequiel Juliano Müller <ezequieljuliano@gmail.com>
2014-05-02 16:49:10 -0300

    Refactoring

--- src/PropertiesFile4D.pas
+++ src/PropertiesFile4D.pas
@@ -1,5 +1,5 @@
 (*
-  Copyright 2013 Ezequiel Juliano Müller - ezequieljuliano@gmail.com
+  Copyright 2014 Ezequiel Juliano Müller | Microsys Sistemas Ltda
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 *)
-
 unit PropertiesFile4D;
 
 interface
--- unittest/PropertiesFile4DTests.dpr
+++ unittest/PropertiesFile4DTests.dpr
@@ -1,11 +1,11 @@
 (*
-  Copyright 2013 Ezequiel Juliano Müller - ezequieljuliano@gmail.com
+  Copyright 2014 Ezequiel Juliano Müller | Microsys Sistemas Ltda
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
 
-            http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
   limitations under the License.
 *)
 
-program PropertiesFile4D.Tests;
+program PropertiesFile4DTests;
 {
 
   Delphi DUnit Test Project
